63f0a78a533daf8fc83a9fcf49e90b4eb2360931
kaspernyhus <kanyhus@gmail.com>
2022-11-28 14:37:09 +0100

    change istrings

--- additions/src/usb_descriptors.c
+++ additions/src/usb_descriptors.c
@@ -158,11 +158,11 @@ uint8_t const *tud_descriptor_configuration_cb(uint8_t index)
 char const *string_desc_arr[] =
     {
         (const char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
-        "Bang & Olufsen",                // 1: Manufacturer
-        "Classics Adapter",            // 2: Product
+        "Micnode",                // 1: Manufacturer
+        "test",            // 2: Product
         "123456",                   // 3: Serials, should use chip ID
         "UAC2",                     // 4: Audio Interface
-};
+    };
 
 static uint16_t _desc_str[32];
 
